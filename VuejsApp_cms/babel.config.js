38c154c0eefaba77a6bb53a3878ddfc816a77b13
Nightstars <christzhangowner#gmail.com>
2020-09-15 12:46:31 +0800

    20200915

--- VuejsApp_cms/babel.config.js
+++ VuejsApp_cms/babel.config.js
@@ -1,5 +1,20 @@
+/* eslint-disable comma-dangle */
 module.exports = {
   presets: [
-    '@vue/app'
-  ]
-};
+    [
+      '@vue/app',
+      {
+        useBuiltIns: 'entry'
+      }
+    ]
+  ],
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk',
+      },
+    ],
+  ],
+}
